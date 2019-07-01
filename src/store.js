9ce5d5daa172a2dcc4e42900895c18fe39777129
akozyreva <anna.ko4523546@gmail.com>
2019-07-01 12:31:18 +0200

    fix key

--- src/store.js
+++ src/store.js
@@ -15,7 +15,7 @@ export default new Vuex.Store({
   },
   actions: {
     setPhotos (vuexContext, page) {
-      const api_key = "a78365b0b0a18733a5419c9da4e39706"
+      const api_key = "588b8e660281831f155e54708f321d37"
       const baseURI = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=${api_key}&per_page=3&page=${page}&format=json&nojsoncallback=1`
         axios.get(baseURI)
         .then( result => {
